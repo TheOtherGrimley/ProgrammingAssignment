99b260e37a0e08cd51bf8e611fbe07934f48e28e
McCabe-D16@email.ulster.ac.uk <B00691355@scm.net>
2016-11-24 15:39:40 +0000

    a2

---
+++ src/concertbooking/ConcertBooking.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package concertbooking;
+
+/**
+ *
+ * @author B00691355
+ */
+public class ConcertBooking {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
