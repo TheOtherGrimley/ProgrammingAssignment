3e5e44a8001e41a8f1e30e07f033d2e263b86010
Adam G <adamgrimley@outlook.com>
2016-12-03 14:49:27 +0000

    a30

--- src/concertbooking/Scan.java
+++ src/concertbooking/Scan.java
@@ -7,11 +7,14 @@ package concertbooking;
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -32,8 +35,7 @@ public class Scan {
         seatDetails.useDelimiter(",");
         
         
-        concertDetails = new Scanner(new File(System.getProperty("user.dir") + "/src/concertbooking/ConcertDetails.txt"));
-        concertDetails.useDelimiter(",");
+        
         
         
         }
@@ -41,6 +43,31 @@ public class Scan {
         
         fillSeats();
     }
+    
+    public String[] readConcert(){
+        try{
+            concertDetails = new Scanner(new File(System.getProperty("user.dir") + "/src/concertbooking/ConcertDetails.txt"));
+            concertDetails.useDelimiter(",");
+            String[] cDetails = new String[5];
+            while(concertDetails.hasNextLine()){
+                cDetails[0] = concertDetails.next();
+                cDetails[1] = concertDetails.next();
+                cDetails[2] = concertDetails.next();
+                cDetails[3] = concertDetails.next();
+                cDetails[4] = concertDetails.next();
+                concertDetails.nextLine();
+            }
+            
+            concertDetails.close();
+            return cDetails;
+        }
+        catch(Exception e){}
+        
+        return null;
+        
+        
+    }
+    
     
     public void onClose(){
         try{
@@ -57,33 +84,70 @@ public class Scan {
     }
         
     private void fillSeats(){
-        for(int i = 0; i < 90; i++){
-            if(i>=0 && i<30){
-                seatArray[i] = new GoldSeat();
-                seatArray[i].setSeatNumber(seatDetails.next());
-                seatArray[i].setSeatTier(seatDetails.next());
-                seatArray[i].setCustomerName(seatDetails.next());
-                seatArray[i].setExtra(seatDetails.next());
-                seatDetails.nextLine();
+        try {
+            for (int i = 0; i < 90; i++) {
+                if (i >= 0 && i < 30) {
+                    seatArray[i] = new GoldSeat();
+                    seatArray[i].setSeatNumber(seatDetails.next());
+                    seatArray[i].setSeatTier(seatDetails.next());
+                    seatArray[i].setCustomerName(seatDetails.next());
+                    seatArray[i].setExtra(seatDetails.next());
+                    seatDetails.nextLine();
+                }
+                if (i >= 30 && i < 60) {
+                    seatArray[i] = new SilverSeat();
+                    seatArray[i].setSeatNumber(seatDetails.next());
+                    seatArray[i].setSeatTier(seatDetails.next());
+                    seatArray[i].setCustomerName(seatDetails.next());
+                    seatArray[i].setExtra(seatDetails.next());
+                    seatDetails.nextLine();
+                }
+                if (i >= 60 && i < 90) {
+                    seatArray[i] = new BronzeSeat();
+                    seatArray[i].setSeatNumber(seatDetails.next());
+                    seatArray[i].setSeatTier(seatDetails.next());
+                    seatArray[i].setCustomerName(seatDetails.next());
+                    seatArray[i].setExtra(seatDetails.next());
+                    seatDetails.nextLine();
+                }
             }
-            if(i>=30 && i<60){
-                seatArray[i] = new SilverSeat();
-                seatArray[i].setSeatNumber(seatDetails.next());
-                seatArray[i].setSeatTier(seatDetails.next());
-                seatArray[i].setCustomerName(seatDetails.next());
-                seatArray[i].setExtra(seatDetails.next());
-                seatDetails.nextLine();
+        } catch (Exception e) {
+            int dialogueResult = JOptionPane.showConfirmDialog(null, "Error, please check data file is in the correct format."
+                    + "Would you like to load a blank seat plan file? if no the program will exit", "File read error", 2);
+            if (dialogueResult == 0) {
+                FileInputStream instream = null;
+                FileOutputStream outstream = null;
+
+                try {
+                    File in = new File(System.getProperty("user.dir") + "/src/concertbooking/DefaultSeats.txt");
+                    File out = new File(System.getProperty("user.dir") + "/src/concertbooking/SeatDetails.txt");
+
+                    instream = new FileInputStream(in);
+                    outstream = new FileOutputStream(out);
+
+                    byte[] buffer = new byte[1024];
+                    int length;
+
+                    while ((length = instream.read(buffer)) > 0) {
+                        outstream.write(buffer, 0, length);
+                    }
+
+                    instream.close();
+                    outstream.close();
+                    
+                    seatDetails = new Scanner(new File(System.getProperty("user.dir") + "/src/concertbooking/SeatDetails.txt"));
+                    seatDetails.useDelimiter(",");
+                    
+                    fillSeats();
+                    
+                    
+                } catch (Exception j) {
+                }
             }
-            if(i>=60 && i<90){
-                seatArray[i] = new BronzeSeat();
-                seatArray[i].setSeatNumber(seatDetails.next());
-                seatArray[i].setSeatTier(seatDetails.next());
-                seatArray[i].setCustomerName(seatDetails.next());
-                seatArray[i].setExtra(seatDetails.next());
-                seatDetails.nextLine();
-            }
+            
+            else {System.exit(0);
         }
-        
-    
+
     }
+}
 }
